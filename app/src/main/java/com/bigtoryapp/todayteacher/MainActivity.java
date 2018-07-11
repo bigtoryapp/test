dfaa12556a131e114eaf55606cfea505681b0b0c
bigtory <bigtoryapp@gmail.com>
2018-07-11 23:24:19 +0900

    test commit

--- app/src/main/java/com/bigtoryapp/todayteacher/MainActivity.java
+++ app/src/main/java/com/bigtoryapp/todayteacher/MainActivity.java
@@ -2,12 +2,24 @@ package com.bigtoryapp.todayteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        textView.toString();
     }
 }
